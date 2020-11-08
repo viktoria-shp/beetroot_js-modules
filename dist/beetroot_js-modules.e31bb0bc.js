eb9a8b3acdfa56a201ec97ba708b77716c5ef12d
viktoria-shp <vvitashp@gmail.com>
2020-11-08 12:47:48 +0200

    cicle

--- dist/beetroot_js-modules.e31bb0bc.js
+++ dist/beetroot_js-modules.e31bb0bc.js
@@ -344,6 +344,198 @@ $(function () {
     var date = new Date(prompt('Enter Date (yyyy-mm-dd)'));
     date.setDate(date.getDate() + 1);
     alert(date);
+  }); //Cicle
+  //task_1
+
+  $("#g3t1").on("click", function () {
+    var a = Number(prompt("Enter the beginning of the range")),
+        n = Number(prompt("Enter the end of the range")),
+        sum = 0;
+
+    while (a <= n) {
+      sum += a;
+      a++;
+    }
+
+    alert(sum);
+  }); //task_2
+
+  $("#g3t2").on("click", function () {
+    var a = prompt("Enter the first number"),
+        b = prompt("Enter the second number");
+
+    var gcd = function gcd(a, b) {
+      if (b == 0) {
+        return a;
+      }
+
+      return gcd(b, a % b);
+    };
+
+    alert("The greatest common divisor for numbers  ".concat(a, " and ").concat(b, " is ").concat(gcd(parseInt(a), parseInt(b))));
+  }); //task_3
+
+  $("#g3t3").on("click", function () {
+    var number = parseInt(prompt("Enter the number")),
+        divisors = [];
+
+    for (var i = 1; i <= number; i++) {
+      if (number % i == 0 && number !== i && i !== 1) {
+        divisors.push(i);
+      }
+    }
+
+    if (divisors.length) {
+      alert("Divisors for number ".concat(number, "  is ").concat(divisors.join(', '), "."));
+    } else {
+      alert("There are not divisors for number ".concat(number, "."));
+    }
+  }); //task_4
+
+  $("#g3t4").on("click", function () {
+    var numb1 = parseInt(prompt("Enter number : "));
+
+    function getDigitAmount(n) {
+      var c = 1;
+
+      while ((n /= 10) >= 1) {
+        c++;
+      }
+
+      return c;
+    }
+
+    alert(getDigitAmount(numb1));
+  }); //task_5
+
+  $("#g3t5").on("click", function () {
+    var countNumbers = 4;
+    var numbers = [];
+    var c = 1;
+
+    while (c <= countNumbers) {
+      var number = parseInt(prompt("Enter ".concat(c, " number : ")));
+      numbers.push(number);
+      c++;
+    }
+
+    var negativeNumbersCount = 0;
+    var positiveNumbersCount = 0;
+    var oddNumbers = 0;
+    var evenNumbers = 0;
+    var usernul = 0;
+    numbers.forEach(function (number) {
+      if (number == 0) {
+        usernul++;
+      }
+
+      if (number < 0) {
+        negativeNumbersCount++;
+      } else {
+        positiveNumbersCount++;
+      }
+
+      if (number % 2 == 0) {
+        evenNumbers++;
+      } else {
+        oddNumbers++;
+      }
+    });
+    alert("Entered numbers ".concat(numbers, " Null ").concat(usernul, " PositiveNumbers ").concat(positiveNumbersCount, ", NegativeNumbers ").concat(negativeNumbersCount, ", EvenNumbers ").concat(evenNumbers, ", OddNumbers ").concat(oddNumbers));
+  }); //task_6
+
+  $("#g3t6").on("click", function () {
+    var _loop = function _loop() {
+      var a = +prompt('Enter first number');
+      var b = +prompt('Enter second number');
+      var op = prompt('Select a character - + / *');
+      var action = {
+        '+': function _() {
+          return a + b;
+        },
+        '-': function _() {
+          return a - b;
+        },
+        '/': function _() {
+          return a / b;
+        },
+        '*': function _() {
+          return a * b;
+        }
+      }[op];
+
+      if (action) {
+        alert(action());
+      } else {
+        alert("Wrong sign");
+      }
+
+      if (!confirm('Do you want to solve another example?')) return "break";
+    };
+
+    while (true) {
+      var _ret = _loop();
+
+      if (_ret === "break") break;
+    }
+  }); //task_7
+
+  $("#g3t7").on("click", function () {
+    var number = prompt("Enter number");
+    var a = prompt("How many digits should I move it?");
+    alert(number.replace(RegExp("(\\d{" + a + "})(\\d*)$"), "$2$1"));
+  }); //task_8
+
+  $("#g3t8").on("click", function () {
+    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    var currDay = 0;
+
+    while (confirm("".concat(days[currDay], ". Do you want to see next day?"))) {
+      currDay = (currDay + 1) % days.length;
+    }
+  }); //task_9
+
+  $("#g3t9").on("click", function () {
+    var cd = '<table cellpadding="6" cellspacing="3" border="1">\n';
+    cd += '<tr>\n';
+
+    for (var i = 1; i <= 9; i++) {
+      cd += '<td align="right"><font color="red">&nbsp;' + (i == 1 ? '' : i) + '</font></td>\n';
+    }
+
+    for (i = 1; i <= 10; i++) {
+      cd += '<tr>\n';
+
+      for (var j = 1; j <= 9; j++) {
+        cd += '<td align="right">' + (j == 1 ? '<font color="red">&nbsp;' + i + '</font>' : i * j) + '</td>\n';
+      }
+
+      cd += '</tr>\n';
+    }
+
+    cd += '</table>';
+    with (document) open(), write(cd), close();
+  }); //task_10
+
+  $("#g3t10").on("click", function () {
+    var left = 1,
+        current,
+        is_right,
+        right = 100;
+
+    while (1) {
+      current = parseInt((left + right) / 2);
+      is_right = prompt("Your number:".concat(current, "?(=, >, <)"));
+
+      if (is_right == '=') {
+        alert("I guess your number. Your number is ".concat(current, "."));
+        break;
+      } else if (is_right == '>') {
+        left = current + 1;
+      } else if (is_right == '<') {
+        right = current - 1;
+      }
+    }
   });
 });
 $('.accordion-toggle').click(function (e) {
@@ -390,7 +582,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52909" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49186" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -210,12 +210,172 @@ $(function(){
         date.setDate(date.getDate() + 1);
         alert( date )
     });
+
+
+    //Cicle
+
+    //task_1
+    $("#g3t1").on("click", function(){
+        let a = Number(prompt("Enter the beginning of the range")),
+            n = Number(prompt("Enter the end of the range")),
+            sum = 0;        
+        while(a <= n){       
+            sum+=a;  
+            a++;
+            }        
+        alert(sum)
+    });
+    //task_2
+    $("#g3t2").on("click", function(){
+        let a = prompt("Enter the first number"),
+            b = prompt("Enter the second number");
+        let gcd = function(a, b) {
+            if ( b == 0) {
+                return a;
+            }
+        
+            return gcd(b, a % b);
+        };
+        alert(`The greatest common divisor for numbers  ${a} and ${b} is ${gcd(parseInt(a), parseInt(b))}`);
+    });
+    //task_3
+    $("#g3t3").on("click", function(){
+        let number = parseInt(prompt("Enter the number")),
+            divisors = [];
+        for (let i = 1; i <= number; i ++) {
+            if (number%i == 0 && number!==i && i!==1) {
+                divisors.push(i);
+            }
+        }    
+        if (divisors.length) {
+            alert(`Divisors for number ${number}  is ${divisors.join(', ')}.`);
+        }else {
+            alert(`There are not divisors for number ${number}.`)
+        }
+        
+    });
+    //task_4
+    $("#g3t4").on("click", function(){
+        const numb1 = parseInt(prompt("Enter number : ", ));
+        function getDigitAmount(n) {
+            let c = 1;
+            while (( n /= 10)>=1)
+                c++;
+            return (c);
+        }
+        alert( getDigitAmount(numb1));
+    });
+    //task_5
+    $("#g3t5").on("click", function(){
+        const countNumbers = 4;
+        let numbers = [];
+        let c = 1;
+        while (c <= countNumbers) {
+            const number = parseInt(prompt(`Enter ${c} number : ` ));
+            numbers.push(number);
+            c++;
+        }
+        let negativeNumbersCount = 0;
+        let positiveNumbersCount = 0;
+        let oddNumbers = 0;
+        let evenNumbers = 0;
+        let usernul = 0;
+        numbers.forEach(function(number) {
+            if (number ==0) {
+                usernul++;
+            }
+            if (number < 0) {
+                negativeNumbersCount++;
+            } else {
+                positiveNumbersCount++;
+            }
+            if (number % 2 == 0) {
+                evenNumbers++;
+            } else {
+                oddNumbers++;
+            }
+        });
+        alert(`Entered numbers ${numbers} Null ${usernul} PositiveNumbers ${positiveNumbersCount}, NegativeNumbers ${negativeNumbersCount}, EvenNumbers ${evenNumbers}, OddNumbers ${oddNumbers}`);
+    });
+    //task_6
+    $("#g3t6").on("click", function(){
+        while (true) {
+            const a = +prompt('Enter first number');
+            const b = +prompt('Enter second number');
+            const op = prompt('Select a character - + / *');
+            const action = {
+              '+': () => a + b,
+              '-': () => a - b,
+              '/': () => a / b,
+              '*': () => a * b
+            }[op];
+            if (action){
+                alert(action());
+            } else {
+                alert (`Wrong sign`)
+            }          
+            if (!confirm('Do you want to solve another example?')) break;
+          }
+    });
+    //task_7
+    $("#g3t7").on("click", function(){
+        let number = prompt(`Enter number`);
+        let a = prompt(`How many digits should I move it?`); 
+        alert(number.replace(RegExp("(\\d{" + a + "})(\\d*)$"), "$2$1"));
+
+    });
+    //task_8
+    $("#g3t8").on("click", function(){
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        let currDay = 0;
+        while (confirm(`${days[currDay]}. Do you want to see next day?`)) {
+        currDay = (currDay + 1) % days.length;
+        }
+    });
+    //task_9
+    $("#g3t9").on("click", function(){
+        var cd  = '<table cellpadding="6" cellspacing="3" border="1">\n';
+            cd += '<tr>\n';
+        for (var i = 1; i <= 9; i++) 
+            cd += '<td align="right"><font color="red">&nbsp;' + ((i == 1) ? '' : i) + '</font></td>\n';
+        for (i = 1; i <= 10; i++)
+            {
+            cd += '<tr>\n';
+            for (var j = 1; j <= 9; j++)
+            cd += '<td align="right">' + ((j == 1) ? ('<font color="red">&nbsp;' + i + '</font>') : i * j) + '</td>\n';
+            cd += '</tr>\n';
+            }
+            cd += '</table>';
+        with (document) open (), write (cd), close ();
+    });
+    //task_10
+    $("#g3t10").on("click", function(){
+        let left = 1, 
+            current, 
+            is_right,
+            right = 100;
+        while (1){
+            current = parseInt((left+right)/2);
+            is_right = prompt( `Your number:${current}?(=, >, <)`);
+            if (is_right == '=') {
+                alert(`I guess your number. Your number is ${current}.`);
+                break;
+            } else if(is_right =='>') {
+                left = current + 1;
+            }
+            else if (is_right =='<') {
+                right = current - 1;
+            }
+        }
+    });
 });
+
+
 $('.accordion-toggle').click(function(e) {
     e.preventDefault();  
   $("a:first-of-type").removeClass('chev');
   
-  var $this = $(this);
+  let $this = $(this);
   if ($this.next().hasClass('show')) {
       $this.next().removeClass('show');
       $this.next().slideUp(350);
@@ -227,3 +387,4 @@ $('.accordion-toggle').click(function(e) {
       $this.next().slideToggle(350);
   }
 });
+
